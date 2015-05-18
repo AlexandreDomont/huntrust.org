43ea27df8c1a2419faa638f5d49e1dc3c5a5d900
Alexandre Domont <alexandre@domont.org>
2015-05-18 18:47:09 +0200

    sitemap generator + robots.txt ++

--- sitemaps.py
+++ sitemaps.py
@@ -9,10 +9,10 @@ Fichier = open(NomFichier,'w')
 Fichier.write('<?xml version="1.0" encoding="UTF-8"?>')
 Fichier.write('\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
 Fichier.write('\n<url>')
-Fichier.write('\n<loc>http://hunstrust.org/</loc>')
+Fichier.write('\n<loc>http://huntrust.org/</loc>')
 Fichier.write('\n</url>')
 Fichier.write('\n<url>')
-Fichier.write('\n<loc>http://hunstrust.org/about.html</loc>')
+Fichier.write('\n<loc>http://huntrust.org/about.html</loc>')
 Fichier.write('\n</url>')
 
 
@@ -23,7 +23,7 @@ for file in glob.iglob("/var/www/huntrust/blog/articles/*.html"):
         	#out.append(article)
 		print article
 		Fichier.write('\n<url>')
-		Fichier.write('\n<loc>http://hunstrust.org/'+ article + '</loc>')
+		Fichier.write('\n<loc>http://huntrust.org/articles/'+ article + '</loc>')
 		Fichier.write('\n</url>')
 
 Fichier.write('\n</urlset>')
