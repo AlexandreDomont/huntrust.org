f97129187a88f83f9c2968fb31730ce182212601
Alexandre Domont <alex@6lex.org>
2017-02-06 16:48:46 +0000

    restructure....

--- sitemaps.py
+++ sitemaps.py
@@ -8,16 +8,16 @@ Fichier = open(NomFichier,'w')
 Fichier.write('<?xml version="1.0" encoding="UTF-8"?>')
 Fichier.write('\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
 Fichier.write('\n<url>')
-Fichier.write('\n<loc>http://huntrust.org/</loc>')
+Fichier.write('\n<loc>http://domont.org/</loc>')
 Fichier.write('\n</url>')
 Fichier.write('\n<url>')
-Fichier.write('\n<loc>http://huntrust.org/about-alexandre-domont.html</loc>')
+Fichier.write('\n<loc>http://domont.org/about-alexandre-domont.html</loc>')
 Fichier.write('\n</url>')
 
-for file in glob.iglob("/var/www/huntrust/blog/articles/*.html"):
+for file in glob.iglob("/var/www/html/blog/blog/articles/*.html"):
         	article = file[32:]
 		Fichier.write('\n<url>')
-		Fichier.write('\n<loc>http://huntrust.org/articles/'+ article + '</loc>')
+		Fichier.write('\n<loc>http://domont.org/articles'+ article + '</loc>')
 		Fichier.write('\n</url>')
 
 Fichier.write('\n</urlset>')
